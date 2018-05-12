f3a921e210901618e3b28f7d87dc590480682bfb
Amanda Albuquerque <amandlei@hotmail.com>
2018-05-12 20:29:10 +0000

    report: add page

--- packages/pilot/src/pages/LoggedArea/routes.js
+++ packages/pilot/src/pages/LoggedArea/routes.js
@@ -1,6 +1,8 @@
 import Transaction32 from 'emblematic-icons/svg/Transaction32.svg'
+import Report32 from 'emblematic-icons/svg/Report32.svg'
 
 import Transactions from '../Transactions'
+import Report from '../Report'
 
 export default {
   transactions: {
@@ -13,6 +15,13 @@ export default {
   transactionsDetails: {
     title: 'transactions.details',
     path: '/transactions/:id',
+    exact: true,
+  },
+  report: {
+    title: 'Relatórios',
+    path: '/report',
+    component: Report,
+    icon: Report32,
     exact: true,
   },
 }
